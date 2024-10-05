c4bdac8306db306dd8050b3190682ba3d24c279c
Softwipe <104397699+SoftW1pe@users.noreply.github.com>
2024-10-05 17:19:16 +0300

    Cвятые угодники...

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-print("левушкка конфетки для моей брюнетки, жизнь без сигаретки, как пюре без котлетки "
-      "если ты заебался сидеть на парах, то просто встань ")
+from aiogram import Bot, Dispatcher, executor, types
+TOKEN = '7402051024:AAFhS1emEY1NMv0MxZAw3RusYi1ZdtTJ-ck'
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+@dp.message_handler(commands=['start']) #Явно указываем в декораторе, на какую команду реагируем.
+async def send_welcome(message: types.Message):
+   await message.reply("Привет!") #Так как код работает асинхронно, то обязательно пишем await.
+@dp.message_handler()
+async def echo(message: types.Message): #Создаём функцию с простой задачей — отправить обратно тот же текст, что ввёл пользователь.
+   await message.answer(message.text)
+if __name__ == '__main__':
+   executor.start_polling(dp, skip_updates=True)
